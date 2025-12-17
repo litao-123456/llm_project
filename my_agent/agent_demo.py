9e2d6e1b409c0764bffbe4d93da120303609afed
LiTao <mzss970625@163.com>
2025-12-17 18:46:10 +0800

    增加 agent 代码

--- my_agent/agent_demo.py
+++ my_agent/agent_demo.py
@@ -1,9 +1,7 @@
-from langchain.agents import *
-from langchain_classic.chains.summarize.refine_prompts import prompt_template
+import json
+
+from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
-from langchain_openai import ChatOpenAI  # 推荐新写法
-from langchain_core.prompts import PromptTemplate
-from langchain_core.runnables import RunnableSequence
 from my_agent import query_api
 import yaml
 
@@ -22,17 +20,15 @@ class AgentDemo:
         )
         return llm
 
-
     @classmethod
-    def get_prompt(self,template_name):
-        with open("D:\python_code\my_code\FastAPIProject\env\prompt.yml",'r',encoding='utf-8') as f:
+    def get_prompt(self, template_name):
+        with open("D:\python_code\my_code\FastAPIProject\env\prompt.yml", 'r', encoding='utf-8') as f:
             prompts = yaml.safe_load(f)
         return prompts.get(template_name)
 
     @classmethod
     def get_llm(cls, query):
         query_type = cls.get_prompt(template_name="query_type")
-        print(query_type)
         prompt = PromptTemplate(
             input_variables=["query"],
             template=query_type,
@@ -40,14 +36,35 @@ class AgentDemo:
         llm = cls.get_langchain_llm()
         formatted_prompt = prompt.format(query=query)
         type_llm = llm.invoke(formatted_prompt)
-        print(type_llm.content)
+        result = json.loads(type_llm.content)
+        print(result)
+        data_type = result.get("type")
+        llm_data = result.get("data")
+        print(llm_data)
+        res = {}
+        # 根据type 获取对应的 tool
+        if "car" in data_type:
+            res = query_api.query_car_code(llm_data)
+        elif "flower" in data_type:
+            res = query_api.query_flower(llm_data)
+        elif "animal" in data_type:
+            res = query_api.query_dog_api(llm_data)
+        # 根据type 获取不同的prompt
+        detail_prompt = cls.get_prompt(template_name=data_type)
+        format_prompt = detail_prompt.format(query=res.get("query"), detail=res.get("detail"))
+        print(format_prompt)
+        detail_llm = llm.stream(format_prompt)
+        all_str = ""
+        for chunk in detail_llm:
+            print(f"data:{chunk.content}")
+            all_str += chunk.content
+        print(f"完整的输出为：{all_str}")
+
 
 if __name__ == '__main__':
     agent = AgentDemo()
-    query = "帮我看下这个车牌号是什么情况，京A98754"
+    query = "加菲猫怎么样？"
     agent.get_llm(query)
-
-
 
 """
 第一步，初始化prompt,读取prompt的文件
--- my_agent/query_api.py
+++ my_agent/query_api.py
@@ -5,17 +5,18 @@ def query_dog_api(name):
     response = request("GET", f" https://api.jisuapi.com/pet/query?appkey=62aa245c6359922e&name={name}")
     response = response.json().get("result")
     rep_json = {
-        "dog_name":response["name"],
+        "query":response["name"],
         "detail":response["message"]
     }
     return rep_json
 
 def query_flower(name):
     response = request(method="GET",url=f"https://api.jisuapi.com/flower/query?appkey=62aa245c6359922e&name={name}")
+    print(response.json())
     response = response.json().get("result")
     rep_json = {
-        "flower_name":response["name"],
-        "floral_lang":response["floral_lang"]
+        "query":response["name"],
+        "detail":response["floral_lang"]
     }
     return rep_json
 
@@ -23,14 +24,14 @@ def query_car_code(code):
     response = request(method="GET",url=f"https://api.jisuapi.com/lsplateluck/query?lsplate={code}&appkey=62aa245c6359922e")
     response = response.json().get("result")
     rep_json = {
-        "car_code":response["lsplate"],
-        "car_detail":response["characterdetail"],
+        "query":response["lsplate"],
+        "detail":response["characterdetail"],
     }
     return rep_json
 
 if __name__ == '__main__':
     print(query_dog_api("德国牧羊犬"))
     print(query_flower("狗尾巴草"))
-    print(query_car_code("冀A97EK6"))
+    print(query_car_code("冀A88888"))
 
 
